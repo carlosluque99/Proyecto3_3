0fef6f124a82cd28ba3357044bf4975a569a3d9e
carlosluque99 <carlosluque99@outlook.es>
2024-11-01 21:17:48 +0100

    Update Clase3_3.java

--- Clase3_3.java
+++ Clase3_3.java
@@ -4,7 +4,7 @@ public class Clase3_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Practica 3_3 CarlosLuqueMiranda");
+		System.out.println("CanelaEnRama");
 
 	}
 
